be58de72653c2673f94d66368310f409c3682eae
Noura <contact.nouraloudani@gmail.com>
2021-01-11 15:12:13 -0500

    commit tree 4c79fa58ac91b67e3c90543c82a12ad1c365fa95

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+
+var customerName = 'bob'
+const leastFavoriteCustomer = "lili"
+var bestCustomer
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "lala"
+}
+
